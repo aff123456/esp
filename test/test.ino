94d0593ef0cb827382d4638a5db5b8272f74e24d
aff123456 <gabri.serratine@gmail.com>
2020-11-17 15:18:41 -0300

    criada função broadcast() para envio de pacotes udp

--- test/test.ino
+++ test/test.ino
@@ -1,11 +1,16 @@
 #include <ESP8266WiFi.h>          // Replace with WiFi.h for ESP32
 #include <ESP8266WebServer.h>     // Replace with WebServer.h for ESP32
 #include <AutoConnect.h>
+#include <WiFiUdp.h>      // biblioteca udp
+ 
+WiFiUDP udp;              // cria um objeto da classe UDP
+
+IPAddress serverIP(255,255,255,255);
+IPAddress broadcastIP(255,255,255,255);
+int port = 5001;
 
 ESP8266WebServer Server;          // Replace with WebServer for ESP32
 AutoConnect      Portal(Server);
-
-//int i = 0;
 
 void rootPage() {
   char content[] = "Hello, world";
@@ -22,10 +27,42 @@ void setup() {
   if (Portal.begin()) {
     Serial.println("WiFi connected: " + WiFi.localIP().toString());
   }
+  // serverIP == broadcastIP quer dizer que o cliente não sabe o IP do servidor
+  while(serverIP == broadcastIP) {
+    broadcast();    // manda um pacote e vê se tem resposta
+    delay(500);     // espera 500ms
+  }
+  Serial.println("Servidor conectado, ip: " + serverIP.toString());
 }
 
 void loop() {
   Portal.handleClient();
-  //Serial.println(i);
-  //i += 1;
+  // codigo loop
+  
+}
+
+// função que envia pacote e verifica se tem resposta do servidor
+void broadcast() {
+  udp.beginPacket(broadcastIP, port);     // manda um pacote broadcast (mandar pacote para o ip 255.255.255.255
+  udp.write("pfg_ip_broadcast_cl");       // conteúdo do pacote
+  udp.endPacket();                        // pacote terminou de enviar
+  delay(25);                              // espera 25ms pra ver se tem resposta
+  listen();                               // checa pra ver se tem resposta
+}
+
+// checa a porta udp pra ver se tem pacotes disponíveis
+void listen() {
+  String req;                             // string onde vai ficar o conteúdo do que for lido
+  if (udp.parsePacket() > 0) {            // parsePacket() =  tamanho do pacote a ser lido, 0 = não há pacotes a serem lidos (https://www.arduino.cc/en/Reference/WiFiUDPParsePacket)
+    req = "";                             // zera a string em preparação pra mensagem
+    while (udp.available() > 0) {         // available() =  número de bytes/caracteres já recebidos a serem lidos (https://www.arduino.cc/en/Reference/WiFiUDPAvailable)
+      char z = udp.read();                // 
+      req += z;
+    }
+    serverIP = udp.remoteIP();
+    Serial.print("IP server: ");
+    Serial.println(serverIP);
+    digitalWrite(ledconfirma, HIGH);
+    }
+  udp.flush();
 }
